c874eeea1fc85766e6129b805b6a61eeaa56360b
hooty Tsai <u1333132@o365.nuu.edu.tw>
2026-03-11 12:02:19 +0800

    0311/4

---
+++ 0311/firstPHP/0311p.php
@@ -0,0 +1,31 @@
+<?php
+echo "Hello, World!";
+
+echo "<H1> Exercise </H1>";
+
+$a=1234;
+$b=234;
+
+$c=$a+$b;
+$d=$a-$b;
+
+$e=23;
+$f=6;
+$g=$e/$f;
+
+
+$n="國立聯合大學";
+$m="苗栗縣";
+$t="$m $n";
+
+echo "The sum of $a and $b is $c";
+echo "<br>";
+echo "The difference of $a and $b is $d";
+echo "<br>";
+echo "The division of $e and $f is $g";
+
+echo "<br>";
+echo "$t";
+
+
+?>
